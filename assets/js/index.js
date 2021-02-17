43fdb19e9ebebbf0a21af7f33e23eb44e869a80e
janeyrinn <jane.rinn@gmail.com>
2021-02-17 12:40:01 +0000

    Add onClick to Icons

---
+++ assets/js/index.js
@@ -0,0 +1,37 @@
+//4 Functions below will toggle selected text on/off on click, source of learning W3Schools
+
+function toggleLocal() {
+  var element = document.getElementById("toggle-local");
+  if (element.style.display === "none") {
+    element.style.display = "block";
+  } else {
+    element.style.display = "none";
+  }
+}
+
+function toggleDeliveries() {
+  var element = document.getElementById("toggle-del");
+  if (element.style.display === "none") {
+    element.style.display = "block";
+  } else {
+    element.style.display = "none";
+  }
+}
+
+function toggleOrders() {
+  var element = document.getElementById("toggle-orders");
+  if (element.style.display === "none") {
+    element.style.display = "block";
+  } else {
+    element.style.display = "none";
+  }
+}
+
+function toggleQuality() {
+  var element = document.getElementById("toggle-quality");
+  if (element.style.display === "none") {
+    element.style.display = "block";
+  } else {
+    element.style.display = "none";
+  }
+}
