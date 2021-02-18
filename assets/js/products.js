06a16cf1048b2a04b7b2a80163c97584eb72a028
janeyrinn <jane.rinn@gmail.com>
2021-02-18 15:27:50 +0000

    Add toggle to search bar

---
+++ assets/js/products.js
@@ -0,0 +1,8 @@
+function toggleSearch() {
+  var element = document.getElementById("search-bar-container");
+  if (element.style.display === "none") {
+    element.style.display = "block";
+  } else {
+    element.style.display = "none";
+  }
+}
