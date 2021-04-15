f85ef4c6498815f5228107847232ff9f8a9b79c1
janeyrinn <jane.rinn@gmail.com>
2021-04-15 11:31:40 +0000

    Fix ID name issue stopping resetForm function from working

--- assets/js/contact.js
+++ assets/js/contact.js
@@ -1,53 +1,53 @@
-//Google Maps JS for API Key : Zooms in on location & adds Marker, code used from Google Maps Documentation 
+//Google Maps JS for API Key : Zooms in on location & adds Marker, code used from Google Maps Documentation
 
 function initMap() {
+  //Location
+  var jpTrading = {
+    lat: 51.91828,
+    lng: -8.18917,
+  };
 
-    //Location
-    var jpTrading = {
-        lat: 51.91828,
-        lng: -8.18917
-    };
+  //Map view
+  var map = new google.maps.Map(document.getElementById("map"), {
+    zoom: 15,
+    center: jpTrading,
+  });
 
-    //Map view
-    var map = new google.maps.Map(document.getElementById("map"), {
-        zoom: 15,
-        center: jpTrading,
-    });
-
-    // The marker pin position
-    var marker = new google.maps.Marker({
-        position: jpTrading,
-        map: map,
-    });
-
+  // The marker pin position
+  var marker = new google.maps.Marker({
+    position: jpTrading,
+    map: map,
+  });
 }
 
 //EmailJS SDK Code: Sends emails to business owner through contact form
 // Code learned and customized from code used by Matt Rudge in "Using EmailJS" CI Tutuorial
 
 function sendMail(contactForm) {
-    emailjs.send("queries_jptrading", "queries_general", {
-        "from_name": contactForm.name.value,
-        "from_email": contactForm.emailaddress.value,
-        "general_query": contactForm.generalquery.value
+  emailjs
+    .send("queries_jptrading", "queries_general", {
+      from_name: contactForm.name.value,
+      from_email: contactForm.emailaddress.value,
+      general_query: contactForm.generalquery.value,
     })
-        //This section will call a modal on un/successful submission of the form
-        .then(
-            function (response) {
-                $('#successModal').modal('show');
-            },
-            function (error) {
-                $('#errorModal').modal('show');
-            });
-    return false;
+    //This section will call a modal on un/successful submission of the form
+    .then(
+      function (response) {
+        $("#successModal").modal("show");
+      },
+      function (error) {
+        $("#errorModal").modal("show");
+      }
+    );
+  return false;
 }
 
 //Below will reset the input fields in the form after clicking the modal button
 function resetForm() {
-    document.getElementById("orderForm").reset();
+  document.getElementById("contact-form").reset();
 }
 
-//Below validates the name so that it must contain more than an escape/space 
+//Below validates the name so that it must contain more than an escape/space
 
 function validateContactForm() {
   var name = document.getElementById("name");
@@ -55,7 +55,7 @@ function validateContactForm() {
   var message = document.getElementById("generalquery");
 
   // if statement validates against empty input and special characters
-  if (name.value == "" || name.value== " ") {
+  if (name.value == "" || name.value == " ") {
     alert("First name required");
     name.focus();
     return false;
@@ -68,7 +68,7 @@ function validateContactForm() {
     return false;
   }
 
-  if (message.value == ""|| message.value == " " ) {
+  if (message.value == "" || message.value == " ") {
     alert("Please enter your enquiry");
     message.focus();
     return false;
